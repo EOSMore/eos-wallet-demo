e20da94f1082669f47f4a92fc8c7412b5b0c1c5b
shrimp <amoy0226@gmail.com>
2018-01-12 13:13:40 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const rewireLess = require('react-app-rewire-less');
+const { injectBabelPlugin } = require('react-app-rewired');
+
+module.exports = function override(config, env) {
+  config = rewireLess(config, env);
+  config = injectBabelPlugin(['import', { libraryName: 'antd-mobile' }], config);
+  return config;
+};
